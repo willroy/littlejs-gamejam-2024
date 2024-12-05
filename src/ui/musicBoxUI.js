8269a443ba767b610d74864957a244ee4ef2a781
Ant <work@antroy.co.uk>
2024-12-05 21:07:10 +0000

    Sequence playing

--- src/ui/musicBoxUI.js
+++ src/ui/musicBoxUI.js
@@ -18,29 +18,41 @@ class MusicBoxUI extends EngineObject {
 
 
     this.keys = [
-      this.player.pos.add(vec2(-2.46, -1.05)),
-      this.player.pos.add(vec2(-1.26, -1.05)),
-      this.player.pos.add(vec2(-0.06, -2.05)),
-      this.player.pos.add(vec2(1.14, -1.05)),
-      this.player.pos.add(vec2(2.36, -1.05)),
+      this.player.pos.add(vec2(-1.86, -3.6)),
+      this.player.pos.add(vec2(-1.26, -3.6)),
+      this.player.pos.add(vec2(-0.65, -3.6)),
+      this.player.pos.add(vec2(-0.05, -3.6)),
+      this.player.pos.add(vec2(0.55, -3.6)),
+      this.player.pos.add(vec2(1.15, -3.6)),
+      this.player.pos.add(vec2(1.75, -3.6)),
     ]
+
+    this.sequence = [0,3,1,6]
 
     this.startButton = this.player.pos.add(vec2(-0.05, 0.46)),
 
     this.startButtonSize = vec2(0.6,0.6);
+    this.keySize = vec2(0.6,1.8);
     this.counter = 0
+    this.key = 0
   }
 
   render() {
     this.image.render();
     
     if (this.playing){
-        drawRect(this.keys[0], this.buttonSize, purple);
-        this.counter++
-        console.log("Count: "+this.counter)
+      drawRect(this.keys[this.sequence[this.key]], this.keySize, purple);
+      this.counter++
       if (this.counter >= 60){
-        this.playing = false
         this.counter = 0
+        this.key++
+        console.log("Key: "+this.key)
+        console.log("Seq: "+this.sequence[this.key])
+      }
+      if (this.key == this.sequence.length){
+        this.key = 0
+        console.log("Stopping tune")
+        this.playing = false
       }
     }
     // drawRect(this.startButton, this.startButtonSize, purple, 0.8)
