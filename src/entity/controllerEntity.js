a161687b8588934a92608ef3bda977f87d7bba38
willroy <willjoroy@gmail.com>
2024-12-14 22:56:24 +0000

    fix move animation bug and make snappier

--- src/entity/controllerEntity.js
+++ src/entity/controllerEntity.js
@@ -35,10 +35,11 @@ class ControllerEntity extends Entity {
     };
 
     this.dir = "down";
+    this.dirKeys = [];
 
     this.animationCount = 0;
     this.animationStep = 0;
-    this.animationSpeed = 20;
+    this.animationSpeed = 15;
     this.animationIdle = false
   }
 
@@ -67,10 +68,17 @@ class ControllerEntity extends Entity {
 
     if (this.world.frozen) return;
 
-    if ( keyIsDown("ArrowLeft") && this.collisonCheck(this.pos.x-this.speed, this.pos.y) ) this.move(this.speed, 0, "left")
-    if ( keyIsDown("ArrowRight") && this.collisonCheck(this.pos.x+this.speed, this.pos.y) ) this.move(0 - this.speed, 0, "right")
-    if ( keyIsDown("ArrowUp") && this.collisonCheck(this.pos.x, this.pos.y+this.speed) ) this.move(0, 0 - this.speed, "up")
-    if ( keyIsDown("ArrowDown") && this.collisonCheck(this.pos.x, this.pos.y-this.speed) ) this.move(0, this.speed, "down")
+    var dirKeys = [];
+
+    if ( keyIsDown("ArrowLeft") ) dirKeys.push("left");
+    if ( keyIsDown("ArrowRight") ) dirKeys.push("right");
+    if ( keyIsDown("ArrowUp") ) dirKeys.push("up");
+    if ( keyIsDown("ArrowDown") ) dirKeys.push("down");
+
+    if ( keyIsDown("ArrowLeft") && this.collisonCheck(this.pos.x-this.speed, this.pos.y) ) this.move(this.speed, 0, dirKeys, "left")
+    if ( keyIsDown("ArrowRight") && this.collisonCheck(this.pos.x+this.speed, this.pos.y) ) this.move(0 - this.speed, 0, dirKeys, "right")
+    if ( keyIsDown("ArrowUp") && this.collisonCheck(this.pos.x, this.pos.y+this.speed) ) this.move(0, 0 - this.speed, dirKeys, "up")
+    if ( keyIsDown("ArrowDown") && this.collisonCheck(this.pos.x, this.pos.y-this.speed) ) this.move(0, this.speed, dirKeys, "down")
 
     if ( !keyIsDown("ArrowLeft") && !keyIsDown("ArrowRight") && !keyIsDown("ArrowUp") && !keyIsDown("ArrowDown") ) this.animationIdle = true;
 
@@ -85,11 +93,12 @@ class ControllerEntity extends Entity {
     }
   }
 
-  move(xdiff, ydiff, dir) {
+  move(xdiff, ydiff, dirKeys, dir) {
     this.world.reposition(xdiff, ydiff);
     this.animationIdle = false;
-    if ( this.dir != dir ) {
+    if ( this.dirKeys.toString() != dirKeys.toString() ) {
       this.dir = dir;
+      this.dirKeys = dirKeys;
       this.animationCount = 0;
       this.animationStep = 0;
       var animationStepXY = vec2(this.animationSteps[this.dir][this.animationStep][0], this.animationSteps[this.dir][this.animationStep][1])
